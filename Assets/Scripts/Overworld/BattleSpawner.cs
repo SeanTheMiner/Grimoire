8ad19f0bd0e4ff386d6b42ad55a9be57d8ff9d14
seanminer27 <seanminer27@gmail.com>
2016-03-03 15:17:53 -0600

    Worked on the overworld a bit, made a trigger to start the battle. Now it's endless. You are Sisyphus. Roll that stone up that hill.

---
+++ Assets/Scripts/Overworld/BattleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BattleSpawner : MonoBehaviour {
+
+    public GameObject player;
+    Rigidbody playerRigidbody;
+
+    void Start() {
+        playerRigidbody = player.GetComponent<Rigidbody>();
+    }
+
+   void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Biome") {
+            SceneManager.LoadScene(sceneName: "Battle");
+        }
+
+    } //end OnTriggerEnter()
+
+} //end BattleSpawner class
