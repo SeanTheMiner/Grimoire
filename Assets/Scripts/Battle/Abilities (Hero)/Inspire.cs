d1b5286e39f15dec9fd06dc87df40cf7aeaa7d16
seanminer27 <seanminer27@gmail.com>
2016-03-31 14:50:34 -0500

    Made the first single-target heal ability...so it turns out that that works.

---
+++ Assets/Scripts/Battle/Abilities (Hero)/Inspire.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inspire : HeroAbility {
+
+    public Inspire() {
+
+        abilityName = "Inspire";
+        abilityType = AbilityType.Burst;
+        targetScope = TargetScope.SingleHero;
+        primaryDamageType = DamageType.Healing;
+        manaCost = 100;
+        
+        chargeDuration = 3.0f;
+        cooldownDuration = 17;
+        procHeal = 220;
+
+    }
+
+    public override void AbilityMap() {
+
+        CheckTarget();
+        HealProcSingle(abilityOwner, targetHero);
+        ApplyEffectSingle(effectApplied, targetHero);
+        ExitAbility();
+
+    } //end AbilityMap()
+
+
+} //end Ability
---
+++ Assets/Scripts/Battle/Effects/InspireEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Effects;
+using BattleObjects;
+
+public class InspireEffect : Effect {
+
+    public InspireEffect() {
+
+        effectName = "Inspire";
+        effectDisplayText = "Armor Up";
+        effectDuration = 8;
+
+    } //end Constructor
+
+    public override void InitEffect(BattleObject host) {
+        base.InitEffect(host);
+        host.armor *= 2.5f;
+    }
+
+    public override void RemoveEffect(BattleObject host) {
+        host.spirit /= 2.5f;
+        base.RemoveEffect(host);
+    }
+
+} //end SpiritBreak class
