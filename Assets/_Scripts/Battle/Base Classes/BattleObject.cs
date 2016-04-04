7f1063977f24c5ad89105ce37bc0f848946267f6
seanminer27 <seanminer27@gmail.com>
2016-04-04 15:34:24 -0500

    Added additive and multiplicative modifiers to stats on BattleObject, but I have yet to have anything use the modifiers.

--- Assets/_Scripts/Battle/Base Classes/BattleObject.cs
+++ Assets/_Scripts/Battle/Base Classes/BattleObject.cs
@@ -9,7 +9,13 @@ namespace BattleObjects {
     //BattleObjects encompass Heroes and Enemies.
     //This way, if something treats both the same, you only have to write code once.
 
+    //Note "AddMod" and "MultMod" - additive modifiers and multiplicative modifiers. 
+    //These are all initialized at 1, and buffs modify these rather than the base stats, 
+    //Which should only be modified by level things? God maybe.
+
     public class BattleObject : MonoBehaviour {
+        
+        //Health & modifiers
 
         public float maxHealth {
             get; set;
@@ -19,43 +25,233 @@ namespace BattleObjects {
             get; set;
         }
 
+
         public float healthRegen {
             get; set;
         }
+
+        public float healthRegenAddMod {
+            get; set;
+        }
+
+        public float healthRegenMultMod {
+            get; set;
+        }
+
+
+        //Offensive stats & modifiers
+        
+        public float physicalPenetration {
+            get; set;
+        }
+
+        public float physicalPenetrationAddMod {
+            get; set;
+        }
+
+        public float physicalPenetrationMultMod {
+            get; set;
+        }
+
+
+        public float physicalAccuracy {
+            get; set;
+        }
+
+        public float physicalAccuracyAddMod {
+            get; set;
+        }
+
+        public float physicalAccuracyMultMod {
+            get; set;
+        }
+
+
+        public float physicalFinesse {
+            get; set;
+        }
+
+        public float physicalFinesseAddMod {
+            get; set;
+        }
+
+        public float physicalFinesseMultMod {
+            get; set;
+        }
+
+
+        public float magicalPenetration {
+            get; set;
+        }
+
+        public float magicalPenetrationAddMod {
+            get; set;
+        }
+
+        public float magicalPenetrationMultMod {
+            get; set;
+        }
+
+
+        public float magicalAccuracy {
+            get; set;
+        }
+
+        public float magicalAccuracyAddMod {
+            get; set;
+        }
+
+        public float magicalAccuracyMultMod {
+            get; set;
+        }
+
+
+        public float magicalFinesse {
+            get; set;
+        }
+
+        public float magicalFinesseAddMod {
+            get; set;
+        }
+
+        public float magicalFinesseMultMod {
+            get; set;
+        }
+
+
+
+        //Defensive stats & modifiers
 
         public float armor {
             get; set;
         }
 
+        public float armorAddMod {
+            get; set;
+        }
+
+        public float armorMultMod {
+            get; set;
+        }
+
+
         public float spirit {
             get; set;
         }
+
+        public float spiritAddMod {
+            get; set;
+        }
+
+        public float spiritMultMod {
+            get; set;
+        }
+
 
         public float physicalEvasionChance {
             get; set;
         }
 
+        public float physicalEvasionChanceAddMod {
+            get; set;
+        }
+
+        public float physicalEvasionChanceMultMod {
+            get; set;
+        }
+
+
         public float magicalEvasionChance {
             get; set;
         }
+        
+        public float magicalEvasionChanceAddMod {
+            get; set;
+        }
+
+        public float magicalEvasionChanceMultMod {
+            get; set;
+        }
+
 
         public float physicalBlockChance {
             get; set;
         }
+        
+        public float physicalBlockChanceAddMod {
+            get; set;
+        }
+
+        public float physicalBlockChanceMultMod {
+            get; set;
+        }
+
 
         public float physicalBlockModifier {
             get; set;
         }
 
+        public float physicalBlockModifierAddMod {
+            get; set;
+        }
+
+        public float physicalBlockModifierMultMod {
+            get; set;
+        }
+
+
         public float magicalBlockChance {
             get; set;
         }
 
+        public float magicalBlockChanceAddMod {
+            get; set;
+        }
+
+        public float magicalBlockChanceMultMod {
+            get; set;
+        }
+
+        
         public float magicalBlockModifier {
             get; set;
         }
 
+        public float magicalBlockModifierAddMod {
+            get; set;
+        }
 
+        public float magicalBlockModifierMultMod {
+            get; set;
+        }
+
+
+        public float physicalLifesteal {
+            get; set;
+        }
+
+        public float physicalLifestealAddMod {
+            get; set;
+        }
+
+        public float physicalLifestealMultMod {
+            get; set;
+        }
+
+
+        public float magicalLifesteal {
+            get; set;
+        }
+        
+        public float magicalLifestealAddMod {
+            get; set;
+        }
+
+        public float magicalLifestealMultMod {
+            get; set;
+        }
+
+        
         public List<Effect> effectList;
         
 
@@ -65,15 +261,74 @@ namespace BattleObjects {
 
             maxHealth = 0;
             currentHealth = 0;
+
             healthRegen = 0;
+            healthRegenAddMod = 1;
+            healthRegenMultMod = 1;
+
+            physicalPenetration = 0;
+            physicalPenetrationAddMod = 1;
+            physicalPenetrationMultMod = 1;
+
+            physicalAccuracy = 0;
+            physicalAccuracyAddMod = 1;
+            physicalAccuracyMultMod = 1;
+
+            physicalFinesse = 0;
+            physicalFinesseAddMod = 1;
+            physicalFinesseMultMod = 1;
+
+            magicalPenetration = 0;
+            magicalPenetrationAddMod = 1;
+            magicalPenetrationMultMod = 1;
+
+            magicalAccuracy = 0;
+            magicalAccuracyAddMod = 1;
+            magicalAccuracyMultMod = 1;
+
+            magicalFinesse = 0;
+            magicalFinesseAddMod = 1;
+            magicalFinesseMultMod = 1;
+            
             armor = 0;
+            armorAddMod = 1;
+            armorMultMod = 1;
+
             spirit = 0;
+            spiritAddMod = 1;
+            spiritMultMod = 1;
+            
             physicalEvasionChance = 0;
+            physicalEvasionChanceAddMod = 1;
+            physicalEvasionChanceMultMod = 1;
+
             magicalEvasionChance = 0;
+            magicalEvasionChanceAddMod = 1;
+            magicalEvasionChanceMultMod = 1;
+
             physicalBlockChance = 0;
+            physicalBlockChanceAddMod = 1;
+            physicalBlockChanceMultMod = 1;
+
             physicalBlockModifier = 0;
+            physicalBlockModifierAddMod = 1;
+            physicalBlockModifierMultMod = 1;
+
             magicalBlockChance = 0;
+            magicalBlockChanceAddMod = 1;
+            magicalBlockChanceMultMod = 1;
+
             magicalBlockModifier = 0;
+            magicalBlockModifierAddMod = 1;
+            magicalBlockModifierMultMod = 1;
+
+            physicalLifesteal = 0;
+            physicalLifestealAddMod = 1;
+            physicalLifestealMultMod = 1;
+
+            magicalLifesteal = 0;
+            magicalLifestealAddMod = 1;
+            magicalLifestealMultMod = 1;
             
         } //end Constructor
 
