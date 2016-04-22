c972fd52c19cd61a9a61055e26b1447290142265
seanminer27 <seanminer27@gmail.com>
2016-04-22 16:14:49 -0500

    Finally went back and made hero effect icons stack vertically. Also added a second row for enemy icons, and a second column for hero icons.

--- Assets/_Scripts/Battle/Controllers/EffectDisplayController.cs
+++ Assets/_Scripts/Battle/Controllers/EffectDisplayController.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using BattleObjects;
+using Enemies;
+using Heroes;
 using Effects;
 
 public class EffectDisplayController : MonoBehaviour {
@@ -10,26 +12,64 @@ public class EffectDisplayController : MonoBehaviour {
     public BattleObject displayHost;
     public Vector3 hostPosition;
 
-    public float iconSpacing;
-    public float iconOffset;
-    public int iconMax;
+    public float enemyIconSpacing, enemyIconHorizontalOffset, enemyIconVerticalOffset,
+        heroIconSpacing, heroIconHorizontalOffset, heroIconVerticalOffset,
+        hoverSpacing;
 
+    public int enemyIconRowMax, heroIconColumnMax;
+    
     public List<GameObject> displayEffectIconList = new List<GameObject>();
     public List<Vector3> displayPositionList = new List<Vector3>();
 
 
 	void Start () {
 
-        iconSpacing = 0.25f;
-        iconOffset = 0.1f;
-        iconMax = 10;
+        enemyIconSpacing = 0.3f;
+        enemyIconHorizontalOffset = 0.1f;
+        enemyIconVerticalOffset = 0;
+        enemyIconRowMax = 10;
+
+        heroIconSpacing = 0.3f;
+        heroIconHorizontalOffset = -0.75f;
+        heroIconVerticalOffset = -0.85f;
+        heroIconColumnMax = 5;
+
+        hoverSpacing = 0.5f;
 
         hostPosition = displayHost.transform.position;
-        
-        for (int i = 1; i <= iconMax; i++) {
-            displayPositionList.Add(new Vector3((hostPosition.x + (i * (iconSpacing)) + iconOffset), (hostPosition.y + 0.5f), hostPosition.z));
-        }
-        
+
+        if (displayHost is Enemy) {
+
+            for (int i = 1; i <= enemyIconRowMax; i++) {
+                displayPositionList.Add(new Vector3((hostPosition.x + (i * (enemyIconSpacing)) + enemyIconHorizontalOffset), 
+                    (hostPosition.y + hoverSpacing), 
+                    (hostPosition.z + enemyIconVerticalOffset)));
+            }
+
+            for (int i = 1; i <= enemyIconRowMax; i++) {
+                displayPositionList.Add(new Vector3((hostPosition.x + (i * (enemyIconSpacing)) + enemyIconHorizontalOffset), 
+                    (hostPosition.y + hoverSpacing), 
+                    (hostPosition.z + enemyIconVerticalOffset + enemyIconSpacing)));
+            }
+
+        } //end if Enemy
+
+        if (displayHost is Hero) {
+
+            for (int i = 1; i <= heroIconColumnMax; i++) {
+                displayPositionList.Add(new Vector3((hostPosition.x + heroIconHorizontalOffset), 
+                    (hostPosition.y + hoverSpacing), 
+                    (hostPosition.z + (i * (heroIconSpacing)) + heroIconVerticalOffset)));
+            }
+
+            for (int i = 1; i <= heroIconColumnMax; i++) {
+                displayPositionList.Add(new Vector3((hostPosition.x + heroIconHorizontalOffset + heroIconSpacing), 
+                    (hostPosition.y + hoverSpacing), 
+                    (hostPosition.z + (i * (heroIconSpacing)) + heroIconVerticalOffset)));
+            }
+
+        } //end if Hero
+     
     } //end Start()
 
 
